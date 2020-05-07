04732a66dffa0baa2137b016eb2b20f4bb88e601
Tim <tim.terf@gmail.com>
2020-05-06 21:46:38 -0400

    properly set up secret env vars with `travis encrypt A=B`

--- docker_push.sh
+++ docker_push.sh
@@ -4,5 +4,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 for arch in amd64 arm64 ppc64le s390x
 do
-    docker push "$TRAVIS_REPO_SLUG:$arch"
+    docker push "${TRAVIS_REPO_SLUG,,}:$arch"
 done
